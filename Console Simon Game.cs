8b6bd8f39126b1bc5a7815ea7c8ef7abb3e18c19
Denis Rafi <62852838+DenisRafi@users.noreply.github.com>
2020-07-29 01:45:36 +0100

    Add files via upload

---
+++ Console Simon Game.cs
@@ -0,0 +1,188 @@
+using System;
+using System.Collections.Generic;
+using System.Threading;
+
+class Program
+{
+	enum Direction
+	{
+		Up = 1,
+		Right = 2,
+		Down = 3,
+		Left = 4,
+	}
+	#region ByDenisRafi
+	static class ByDenisRafi
+	{
+		public static readonly string[] Renders = new string[]
+		{			
+			@"           ╔══════╗        " + '\n' +
+			@"           ║      ║        " + '\n' +
+			@"           ╚╗    ╔╝        " + '\n' +
+			@"    ╔═══╗   ╚╗  ╔╝   ╔═══╗ " + '\n' +
+			@"    ║   ╚═══╗╚══╝╔═══╝   ║ " + '\n' +
+			@"    ║       ║    ║       ║ " + '\n' +
+			@"    ║   ╔═══╝╔══╗╚═══╗   ║ " + '\n' +
+			@"    ╚═══╝   ╔╝  ╚╗   ╚═══╝ " + '\n' +
+			@"           ╔╝    ╚╗        " + '\n' +
+			@"           ║      ║        " + '\n' +
+			@"           ╚══════╝        ",
+			@"           ╔══════╗        " + '\n' +
+			@"           ║██████║        " + '\n' +
+			@"           ╚╗████╔╝        " + '\n' +
+			@"    ╔═══╗   ╚╗██╔╝   ╔═══╗ " + '\n' +
+			@"    ║   ╚═══╗╚══╝╔═══╝   ║ " + '\n' +
+			@"    ║       ║    ║       ║ " + '\n' +
+			@"    ║   ╔═══╝╔══╗╚═══╗   ║ " + '\n' +
+			@"    ╚═══╝   ╔╝  ╚╗   ╚═══╝ " + '\n' +
+			@"           ╔╝    ╚╗        " + '\n' +
+			@"           ║      ║        " + '\n' +
+			@"           ╚══════╝        ",
+			@"           ╔══════╗        " + '\n' +
+			@"           ║      ║        " + '\n' +
+			@"           ╚╗    ╔╝        " + '\n' +
+			@"    ╔═══╗   ╚╗  ╔╝   ╔═══╗ " + '\n' +
+			@"    ║   ╚═══╗╚══╝╔═══╝███║ " + '\n' +
+			@"    ║       ║    ║███████║ " + '\n' +
+			@"    ║   ╔═══╝╔══╗╚═══╗███║ " + '\n' +
+			@"    ╚═══╝   ╔╝  ╚╗   ╚═══╝ " + '\n' +
+			@"           ╔╝    ╚╗        " + '\n' +
+			@"           ║      ║        " + '\n' +
+			@"           ╚══════╝        ",
+			@"           ╔══════╗        " + '\n' +
+			@"           ║      ║        " + '\n' +
+			@"           ╚╗    ╔╝        " + '\n' +
+			@"    ╔═══╗   ╚╗  ╔╝   ╔═══╗ " + '\n' +
+			@"    ║   ╚═══╗╚══╝╔═══╝   ║ " + '\n' +
+			@"    ║       ║    ║       ║ " + '\n' +
+			@"    ║   ╔═══╝╔══╗╚═══╗   ║ " + '\n' +
+			@"    ╚═══╝   ╔╝██╚╗   ╚═══╝ " + '\n' +
+			@"           ╔╝████╚╗        " + '\n' +
+			@"           ║██████║        " + '\n' +
+			@"           ╚══════╝        ",
+			@"           ╔══════╗        " + '\n' +
+			@"           ║      ║        " + '\n' +
+			@"           ╚╗    ╔╝        " + '\n' +
+			@"    ╔═══╗   ╚╗  ╔╝   ╔═══╗ " + '\n' +
+			@"    ║███╚═══╗╚══╝╔═══╝   ║ " + '\n' +
+			@"    ║███████║    ║       ║ " + '\n' +
+			@"    ║███╔═══╝╔══╗╚═══╗   ║ " + '\n' +
+			@"    ╚═══╝   ╔╝  ╚╗   ╚═══╝ " + '\n' +
+			@"           ╔╝    ╚╗        " + '\n' +
+			@"           ║      ║        " + '\n' +
+			@"           ╚══════╝        ",
+		};
+	}
+	#endregion
+	static readonly bool CursorVisible = GetConsoleCursorVisible();
+	static readonly Random random = new Random();
+	static readonly TimeSpan buttonPress = TimeSpan.FromMilliseconds(500);
+	static readonly TimeSpan animationDelay = TimeSpan.FromMilliseconds(200);
+	static int score = 0;
+	static readonly List<Direction> pattern = new List<Direction>();
+
+	static void Main()
+	{
+		Console.Title = "Console Simon Game";
+		try
+		{
+			Console.CursorVisible = false;
+			Clear();
+			Render(ByDenisRafi.Renders[default]);
+			while (true)
+			{
+				Thread.Sleep(buttonPress);
+				pattern.Add((Direction)random.Next(1, 5));
+				AnimateCurrentPattern();
+				for (int i = 0; i < pattern.Count; i++)
+				{
+				GetInput:
+					switch (Console.ReadKey(true).Key)
+					{
+						case ConsoleKey.UpArrow:
+							if (pattern[i] != Direction.Up) goto GameOver;
+							break;
+						case ConsoleKey.RightArrow:
+							if (pattern[i] != Direction.Right) goto GameOver;
+							break;
+						case ConsoleKey.DownArrow:
+							if (pattern[i] != Direction.Down) goto GameOver;
+							break;
+						case ConsoleKey.LeftArrow:
+							if (pattern[i] != Direction.Left) goto GameOver;
+							break;
+						case ConsoleKey.Escape:
+							Console.Clear();
+							Console.Write("Closed.");
+							return;
+						default: goto GetInput;
+					}
+					score++;
+					Clear();
+					Render(ByDenisRafi.Renders[(int)pattern[i]]);
+					Thread.Sleep(buttonPress);
+					Clear();
+					Render(ByDenisRafi.Renders[default]);
+				}
+			}
+		GameOver:
+			Console.Clear();
+			Console.Write("Game Over. Score: " + score + ".");
+		}
+		finally
+		{
+			Console.CursorVisible = CursorVisible;
+		}
+	}
+	static void Clear()
+	{
+		Console.SetCursorPosition(0, 0);
+		Console.WriteLine();
+		Console.WriteLine();
+		int left = Console.CursorLeft;
+		int top = Console.CursorTop;
+		Render(ByDenisRafi.Renders[default]);
+		Console.SetCursorPosition(left, top);
+	}
+	static void AnimateCurrentPattern()
+	{
+		for (int i = 0; i < pattern.Count; i++)
+		{
+			Clear();
+			Render(ByDenisRafi.Renders[(int)pattern[i]]);
+			Thread.Sleep(buttonPress);
+			Clear();
+			Render(ByDenisRafi.Renders[default]);
+			Thread.Sleep(animationDelay);
+		}
+		Clear();
+		Render(ByDenisRafi.Renders[default]);
+	}
+	static void Render(string @string)
+	{
+		int x = Console.CursorLeft;
+		int y = Console.CursorTop;
+		foreach (char c in @string)
+		{
+			if (c is '\n')
+			{
+				Console.SetCursorPosition(x, ++y);
+			}
+			else
+			{
+				Console.Write(c);
+			}
+		}
+	}
+	static bool GetConsoleCursorVisible()
+	{
+		try
+		{
+			return Console.CursorVisible;
+		}
+		catch (PlatformNotSupportedException)
+		{
+			return true;
+		}
+	}
+}
